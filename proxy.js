c603283bdd556e54119116aaaf0aaefbdc31dda6
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2024-08-06 19:55:06 +0530

    Update proxy.js

--- proxy.js
+++ proxy.js
@@ -1,32 +1,47 @@
-const target = {
-  message1: "hello",
-  message2: "everyone",
-};
-
-const handler2 = {
-  get(target, prop, receiver) {
-    if (prop === "message1") {
-      return "Proxy Message";
-    }
-    return Reflect.get(target, prop, receiver);
-  },
-};
-
-const proxy2 = new Proxy(target, handler2);
-
-console.log(proxy2.message1);  // Output: "Proxy Message"
-console.log(proxy2.message2);  // Output: "everyone"
-console.log(proxy2);           // Output: Proxy {message1: "hello", message2: "everyone"}
-
-
 /*
-
-get Trap: The get trap intercepts property access. It checks if the property being accessed is message1. 
-If so, it returns "Proxy Message". Otherwise, it uses Reflect.get to get the property value from the target object.
-
-Reflect.get: This is used to get the property value from the target object in a standard way, ensuring that the default
-behavior is preserved for properties other than message1.
-
-
+Proxy property we have to use in JS, only when we don't want to intercept the property
+of the original Object and want to add something extra.
+So, In this type of case we should think about Proxy.
 
 */
+
+
+// I want to update the user these are deprecated, and I don't want to intercept the original object.
+const deprecatedFontSizes = {
+  xxl: {
+    currentValue: "xxl",
+    alternative: "l",
+  },
+  xl: {
+    currentValue: "xl",
+    alternative: "m",
+  }
+};
+
+
+// This is my original object
+const fontSizes = {
+  xxl: "extra extra large",
+  xl: "extra large",
+  l: "large",
+  m: "medium",
+  s: "small",
+  xs: "extra small",
+};
+
+const proxyOptions = {
+  get: (target, prop, receiver) => {
+    if (deprecatedFontSizes.hasOwnProperty(prop)) {
+      console.warn(`Warning: ${prop} is deprecated, 
+      instead use this ${deprecatedFontSizes[prop].alternative}`)
+    }
+    return Reflect.get(target, prop, receiver);
+  }
+}
+
+
+const proxy = new Proxy(fontSizes, proxyOptions);
+
+console.log(proxy.xxl);
+console.log(proxy.xl);
+
