d19fb3be047e4600ff50eff70a21da5fb4584191
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2024-08-11 16:07:12 +0530

    Create accessibility.js

---
+++ accessibility.js
@@ -0,0 +1,7 @@
+Color Contrast
+User Interface Zoom
+Keyboard Navigation
+Screenreader support
+
+
+color contrast, user interface zoom support, keyboard interaction models, and screenreader support
