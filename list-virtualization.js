8e0184d8324c283399657d6e8843cbc56404d43c
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2024-08-11 16:54:33 +0530

    Create list-virtualization.js

---
+++ list-virtualization.js
@@ -0,0 +1,12 @@
+List Virtualization
+
+https://www.patterns.dev/vanilla/virtual-lists/
+
+It is a technique, suppose we have 10K data in the list,but if we want to show all the data at once, 
+  our webpage might got stuck because of too much data in the UI
+So, we can use this "Virtualizatioin" technique, 
+In this technique , we can show only those data we got fit in the View Port or our screen.
+
+
+  // We can try to implement this using Intersection observer
+
