07f1acbcc2a11be7e33f48875da83dc4ff3d47de
Ankit Kumar <47697582+Ankit8969@users.noreply.github.com>
2024-09-25 01:40:07 +0530

    Update deepclone.js

--- deepclone.js
+++ deepclone.js
@@ -1,23 +1,50 @@
 
-
-const deepClone = (obj) => {
-  if (typeof obj !== "object")  return obj;
-
-  const newObject = Array.isArray(obj) ? [] : {};
-  for (let key in obj) {
-    const value = obj[key];
-    newObject[key] = deepClone(value);
+const person = {
+  name: 'John Doe',
+  age: 30,
+  address: {
+    street: '123 Main St',
+    city: 'New York',
+    country: 'USA',
+    postalCode: '10001',
+    geoLocation: {
+      lat: 40.7128,
+      lon: -74.0060
+    }
+  },
+  hobbies: ['reading', 'traveling', 'coding'],
+  contactInfo: {
+    phone: {
+      home: '555-555-5555',
+      work: '555-555-1234',
+    },
+    email: 'john.doe@example.com'
   }
-  return newObject;
+};
+
+
+function deepClone(obj) {
+  if (typeof obj !== 'object' || Array.isArray(obj)) 
+      return obj;
+
+  let newObj = {};
+  const keys = Object.keys(obj);
+  keys.forEach((key) => {
+    newObj = {
+      ...newObj,
+      [key]: deepClone(obj[key])
+    }
+  })
+  return newObj;
 }
 
-let arr = {
-  name:"Ankit",
-  age:"25"
-};
-let deepArray = deepClone(arr);
 
-arr.name = 'rohan';
-console.log(arr);
-console.log(deepArray);
+let obj = deepClone(person);
+
+obj.address.street = 'ankit';
+console.log(person);
+console.log(obj);
+
+
+
 
